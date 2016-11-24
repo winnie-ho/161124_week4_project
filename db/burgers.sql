5b419f16caa8b2bed011d06684d7032d41f8ffd2
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-11-24 21:49:02 +0000

    refining planning and set up db

---
+++ db/burgers.sql
@@ -0,0 +1,33 @@
+DROP TABLE restaurants;
+DROP TABLE deals;
+DROP TABLE burgers;
+DROP TABLE days;
+
+
+
+
+CREATE TABLE days(
+id SERIAL8 primary key,
+name VARCHAR(255)
+);
+
+CREATE TABLE burgers(
+id SERIAL8 primary key,
+name VARCHAR(255),
+price FLOAT
+);
+
+CREATE TABLE deals(
+id SERIAL8 primary key,
+name VARCHAR(255),
+description TEXT,
+burger_id INT8 references burgers(id),
+day_id INT8 references days(id)
+);
+
+CREATE TABLE restaurants(
+id SERIAL8 primary key,
+name VARCHAR(255),
+burger_id INT8 references burgers(id),
+deal_id INT8 references deals(id)
+);

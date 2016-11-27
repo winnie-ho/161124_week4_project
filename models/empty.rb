fd9fc9a3d4f3d5184f6238b65141e25c9ead582d
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-11-27 18:23:30 +0000

    more css and tweeks to Empty class

--- models/empty.rb
+++ models/empty.rb
@@ -1,10 +1,10 @@
 class Empty
   def self.all
-    return ["Awww noooo!!", 
-            "Disaster!!", 
-            "WHAT THE ....", 
-            "Sad times! ", 
-            "You're having a giraffe!", 
+    return ["😩Awww noooo!! No deals in the BURGERBELLY on ", 
+            "😖Disaster!! Empty BURGERBELLY on ", 
+            "😡WHAT THE .... Nothing in the BURGERBELLY on ", 
+            "😢Sad times! No deals on ", 
+            "😠You're having a giraffe! BURGERBELLY is bare on ", 
             ]
   end
 end
